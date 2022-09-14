6df194aa420722c8fd61fc32659b79e01cadef0c
Markus Neumann <mneumann.polsci@gmail.com>
2022-09-13 21:24:24 -0400

    fb2022

---
+++ 04_prepare_fb2022.R
@@ -0,0 +1,55 @@
+# Prepare the FB 2022 data so it is in the same shape as the training data
+# Currently does not include disclaimer/ocr
+
+library(data.table)
+library(stringr)
+library(stringi)
+library(dplyr)
+library(tidyr)
+
+# Input data
+path_input_data <- '../datasets/facebook/fb2022_aug8_to_sep11.rdata'
+# Output data
+path_output_data <- "data/fb2022_prepared.csv.gz"
+
+# Load data
+load(path_input_data)
+df$id <- paste0("x_", df$id)
+# Merge in ASR
+asr <- fread("../datasets/facebook/fb2022_asr_0904.csv.gz", colClasses = "character")
+asr <- asr %>% 
+  select(ad_id, google_asr_text) %>%
+  rename(asr = google_asr_text)
+df <- left_join(df, asr, by = c("id" = "ad_id"))
+
+# Clean up brackets
+clean_brackets <- function(x){
+  x <- str_remove(x,  '\\[\\"\\"')
+  x <- str_remove(x,  '\\"\\"\\]')
+  x <- str_replace_all(x,  '\\"\\"', '\\"')
+}
+# Apply the function to all text columns
+df <- df %>%
+  mutate(across(c(ad_creative_body, asr, page_name, 
+                  ad_creative_link_caption, ad_creative_link_title, 
+                  ad_creative_link_description),
+                clean_brackets))
+
+# concatenate them all together
+# Order doesn't matter since we use a bag of words model
+df <- df %>% unite(col = "text", 
+                   c(ad_creative_body, asr, page_name, 
+                     ad_creative_link_caption, ad_creative_link_title, 
+                     ad_creative_link_description), 
+                   sep = " ", na.rm = T)
+
+# Kick out empty ads
+df <- df[df$text != "",]
+df <- df[is.na(df$text) == F,]
+
+# Replace newlines with spaces
+df$text <- str_replace_all(df$text, "\\\n", " ")
+
+df <- df %>% select(id, text)
+
+fwrite(df, path_output_data)
