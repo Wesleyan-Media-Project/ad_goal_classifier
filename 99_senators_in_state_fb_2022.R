fc51951288ec072d641d7f22810cd6eb4682e3bc
Markus Neumann <mneumann.polsci@gmail.com>
2022-10-04 14:18:54 -0400

    fixed problem with scientific notation and changed code to provide the proportion of the senators state rather than whether that prop is above a threshold

--- 99_senators_in_state_fb_2022.R
+++ 99_senators_in_state_fb_2022.R
@@ -8,12 +8,21 @@ library(ggplot2)
 ent <- fread("../datasets/wmp_entity_files/Facebook/2022/wmp_fb_2022_entities_v091922.csv")
 ent <- filter(ent, wmp_spontype == "campaign") %>%
   filter(wmp_office == "us senate") %>%
-  select(pd_id, sen_state)
+  select(pd_id, sen_state) %>%
+  mutate(sen_state = state.name[match(sen_state, state.abb)])
 
 # Load FB 2022 masterfile, only keep id, pdid, delivery_by_region
-fb22 <- fread("../datasets/facebook/fb2022_master_0919_1001.csv.gz")
+fb22 <- fread("../datasets/facebook/fb2022_master_0919_1001.csv.gz", encoding = "UTF-8")
 fb22$id <- paste0("x_", fb22$id)
 fb22 <- fb22 %>% select(id, pd_id, delivery_by_region)
+
+# Merge with WMP entities file
+fb22 <- left_join(fb22, ent, by = "pd_id")
+# Keep only senators
+fb22 <- fb22 %>% filter(is.na(sen_state) == F)
+# Remove ads without a regional distribution
+fb22 <- fb22 %>% filter(is.na(delivery_by_region) == F)
+
 
 # Extract regional impressions (state & percentage)
 # Start extracting the state from region distribution
@@ -24,62 +33,36 @@ fb22$delivery_by_region <-
   str_remove_all('percentage:') %>% 
   str_remove_all(',region') %>% 
   str_remove_all('\\[|\\]|\\{|\\}')
-# Extract the percentage
-fb22$region_pct <- str_extract_all(fb22$delivery_by_region, "[0-9|\\.]+")
-# Finish extracting the state
-fb22$delivery_by_region <- 
-  fb22$delivery_by_region %>%
-  str_remove_all("[0-9|\\:|\\.]") %>%
-  str_split(",") %>%
-  lapply(function(x){state.abb[match(x, state.name)]}) %>%
-  lapply(unique)
 
-# Merge with WMP entities file
-fb22 <- left_join(fb22, ent, by = "pd_id")
-# Keep only senators
-fb22 <- fb22 %>% filter(is.na(sen_state) == F)
-# Remove ads without a regional distribution
-fb22 <- fb22 %>% filter(is.na(delivery_by_region) == F)
+# Then split individual values
+delivery_by_region_split <- str_split(fb22$delivery_by_region, ",")
 
-# Determine in-state
-fb22$instate <- F
-thresholds <- c(0.25, 0.5, 0.75, 0.8, 0.85, 0.9, 0.95, 0.99, 1)
-freqs <- list()
+# Then separate the regions part from the percentages part
+separate_region_from_pct <- function(x){
+  out <- str_split(x, ":")
+  regions <- unlist(lapply(out, function(x){x[2]}))
+  regions_pct <- unlist(lapply(out, function(x){x[1]}))
+  df_regions <- list(regions, regions_pct)
+  return(df_regions)
+}
 
-for (t in 1:length(thresholds)){
-  fb22$instate <- F
-  for(i in 1:nrow(fb22)){
+separated_region_from_pct = lapply(delivery_by_region_split, separate_region_from_pct)
+regions_l = lapply(separated_region_from_pct, function(x){x[1]}) %>% lapply(unlist)
+regions_pct_l = lapply(separated_region_from_pct, function(x){x[2]}) %>% lapply(unlist)
+
+fb22$delivery_by_region <- regions_l
+fb22$region_pct <- regions_pct_l
+
+# Find the proportion of the senator's state; if it isn't present, set it to 0
+fb22$instate_pct <- 0
+for(i in 1:nrow(fb22)){
     # Check which, if any of the targeted states is the senator's
     state_ind <- which(fb22$delivery_by_region[[i]] == fb22$sen_state[i])
     # If any
     if(length(state_ind) != 0){
       # Check if more or equal to threshold
-      fb22$instate[i] <- fb22$region_pct[[i]][state_ind] >= thresholds[t]
+      fb22$instate_pct[i] <- fb22$region_pct[[i]][state_ind]
     }
   }
-  freqs[[t]] <- as.numeric(table(fb22$instate)[2]/nrow(fb22))
-}
-
-df_thresh <- data.frame(thresholds, unlist(freqs))
-names(df_thresh) <- c("thresholds", "freqs")
-ggplot(df_thresh, aes(thresholds, freqs)) + geom_point() + 
-  xlim(0, 1) + ylim(0, 1) +
-  labs(x = "Threshold", y = "Proportion of in-state ads")
-ggsave("figures/instate_threshold.pdf")
-
-
-# Final threshold = 0.95
-fb22$instate <- F
-threshold <- 0.95
-
-for(i in 1:nrow(fb22)){
-  # Check which, if any of the targeted states is the senator's
-  state_ind <- which(fb22$delivery_by_region[[i]] == fb22$sen_state[i])
-  # If any
-  if(length(state_ind) != 0){
-    # Check if more or equal to threshold
-    fb22$instate[i] <- fb22$region_pct[[i]][state_ind] >= threshold
-  }
-}
 
 fwrite(fb22, "data/fb2022_instate_senators.csv")
