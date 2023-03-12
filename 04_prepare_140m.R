fe6731de0d06be1f3bc4f524cf88a4f25b56eee4
Markus Neumann <mneumann.polsci@gmail.com>
2023-03-12 14:54:15 -0400

    inference on 2020 fb 1.4m data

---
+++ 04_prepare_140m.R
@@ -0,0 +1,37 @@
+# Prepare the 128m data so it is in the same shape as the training data
+
+library(data.table)
+library(stringr)
+library(stringi)
+library(dplyr)
+library(tidyr)
+
+# Input data
+path_input_data <- "../fb_2020/fb_2020_140m_adid_text_clean.csv.gz"
+# Output data
+path_output_data <- "data/140m_prepared.csv.gz"
+
+# Read in text data
+df <- fread(path_input_data, encoding = "UTF-8")
+
+# Don't need ad snapshot URL
+df <- df %>% select(-ad_snapshot_url)
+
+# concatenate them all together
+# Order doesn't matter since we use a bag of words model
+df <- df %>% unite(col = "text", 
+                   c(ad_creative_body, aws_ocr_text, google_asr_text, page_name, disclaimer, 
+                     ad_creative_link_caption, ad_creative_link_title, 
+                     ad_creative_link_description), 
+                   sep = " ")
+
+# Kick out empty ads
+df <- df[df$text != "",]
+df <- df[is.na(df$text) == F,]
+
+# Replace newlines with spaces
+df$text <- str_replace_all(df$text, "\\\n", " ")
+# Remove extraneous spaces
+df$text <- str_squish(df$text)
+
+fwrite(df, path_output_data)
---
+++ 05_binomial_goal_clf_inference_fb_140m.py
@@ -0,0 +1,46 @@
+import sklearn.model_selection as ms
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.calibration import CalibratedClassifierCV
+from sklearn import metrics
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+import pandas as pd
+import numpy as np
+from joblib import dump, load
+from tqdm import tqdm
+
+# Input data
+path_140m_acb = 'data/140m_prepared.csv.gz'
+dir_models = 'models/goal_rf_'
+# Ouput data
+path_predictions_gz = 'data/ad_goal_rf_fb_140m.csv.gz'
+
+# Load data
+inference = pd.read_csv(path_140m_acb)
+inference = inference[inference['text'] != ""]
+inference = inference.dropna()
+
+goals = ["DONATE", "CONTACT", "PURCHASE", "GOTV", "EVENT", "POLL", "GATHERINFO", "LEARNMORE", "PRIMARY_PERSUADE"]
+
+for g in tqdm(goals):
+
+  # Load model
+  clf = load(dir_models + g + '.joblib')
+  
+  # Apply clf
+  predicted_prob = clf.predict_proba(inference['text'])
+  predicted = np.argmax(predicted_prob, axis=1)
+  
+  inference['goal_'+g+'_prediction'] = predicted
+  inference['goal_'+g+'_predicted_prob'] = predicted_prob[:,1]
+  
+# Make a column with the largest probability
+inference['goal_highest_prob'] = inference[[col for col in inference.columns if "predicted_prob" in col]].idxmax(1)
+inference['goal_highest_prob'] = inference['goal_highest_prob'].str.replace('_predicted_prob', '')
+inference['goal_highest_prob'] = inference['goal_highest_prob'].str.replace('goal_', '')
+
+# Save without text column
+inference = inference.drop(['text'], 1)
+# inference.to_csv(path_predictions, index = False)
+inference.to_csv(path_predictions_gz, index = False)
